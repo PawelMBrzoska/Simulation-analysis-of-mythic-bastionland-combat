a7b48a710ab9b825a259d2a914843c9cde80f5ba
Paweł Brzóska <pawel.m.brzoska@gmail.com>
2023-05-22 14:06:05 +0200

    Working version 1.0

---
+++ Engine.py
@@ -0,0 +1,86 @@
+#Engine of mechanic based on 18-05-23 version of Mythic Bastionland (https://www.bastionland.com/)
+import random
+import os
+import sys
+
+#Setting directory
+os.chdir(os.path.dirname(sys.argv[0]))
+
+#################################
+#             Classes           #
+#################################
+
+# Warband classes with options from book (p. 9)
+class Warband():
+    def __init__(self, vig, GD, dmg, armor, n = None): # new materials have to be updated here
+        self.vig: int = vig
+        self.GD: int = GD
+        self.dmg: int = dmg
+        self.life: bool = True
+        self.armor:int = armor
+        self.n:int = n
+
+    def __str__(self):
+        return(f"vig = {self.vig}, GD = {self.GD}, dmg = k{self.dmg}. warband{self.n} is alive: {self.life}")
+
+    def recieve_dmg(self, dmg):
+        #print(dmg)
+        dmg -= self.armor
+        if dmg < 0:
+            dmg = 0
+            
+        if dmg < self.GD:
+            self.GD -= dmg
+            #print(dmg, self)
+        else:
+            dmg -= self.GD
+            self.GD = 0
+            #print(dmg, self)
+
+        if dmg >= self.vig/2:
+            self.vig -= dmg
+            self.life = False
+            #print(dmg, self)
+        else:
+            self.vig -= dmg
+            #print(dmg, self)
+
+class Warband_conscripts(Warband):
+    def __init__(self, vig = 10, GD = 4, dmg = 6, armor = 0, n = None):
+
+        super().__init__(vig, GD, dmg, armor, n)
+
+class Warband_mercenaries(Warband):
+    def __init__(self, vig = 10, GD = 5, dmg = 8, armor = 0, n = None):
+
+        super().__init__(vig, GD, dmg, armor, n)
+
+class Warband_knights(Warband):
+    def __init__(self, vig = 13, GD = 7, dmg = 8, armor = 3, n = None):
+
+        super().__init__(vig, GD, dmg, armor, n)
+
+#################################
+#           Functions           #
+#################################
+
+def roll_dmg(*args, bonus = None):
+    dmg = []
+
+    for attacker in args:
+        if attacker.life == True:
+            dmg.append(random.randrange(1, attacker.dmg+1, 1))
+
+    if dmg != [] and bonus != None:
+        dmg.append(random.randrange(1, bonus+1, 1)) #add bonus dice
+
+    if dmg != []: return(max(dmg)) #If multiple attackers return only the biggest value
+    else: return(0)
+
+def deal_dmg_to(*args, dmg):
+    for defender in args:
+        if defender.life == True:
+            defender.recieve_dmg(dmg)
+            dmg = 0
+            break
+
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Simulation-analysis-of-mythic-bastionland-combat
 Small project to check the balance and best tactics in Mythic Bastionland by Chris McDowall
+
+First of all Mythic Bastionland is an Into the Odd hack, both by Chris McDowall (check out his page here: https://www.bastionland.com/).
+
+The Into the Odd mechanic is very light, forcing GMs to make decisions on the fly during sessions. Sometimes the improvised rules are good, sometimes they are bad. 
+My main problem with light mechanics is balance. So I would like to see how the mechanics of Into the Odd and especially Mythic Bastionland work under different conditions.
+
+---
+# 1v1 combat
+In 1v1 the mechanic works perfectly fine. 
+    - Surprise attacks make a big difference.
+    - When fighting simultaneously (e.g., duel) the draws are common 
+
+---
+# Group combat
+The bigger change for me was the mechanic for group combat - when multiple attacks are made against a single enemy, all attack dice are rolled, but only the highest one counts (p. 4). 
+This has specific consequences for the logic of group combat:
+    - A group of small enemies is a bit more dense than a single one.
+    - attacking 1v1 all the time seems to be the most optimal, even if it is unintuitive and "unrealistic xD".
+    - Big armor, big sword > big head count
+    - Big monsters are even harder to kill
+
+And all of this is fair and balanced; however, at the group/warband level, it seems odd to me that outflanking the enemy, which is the most basic tactic for winning in warfare, is now unoptimal. In a series of simulations, it turns out that a bonus to souronding (problably +d8) could balance it, but it is weirdly distributed - it helps in fights between small dmg groups or between groups with big armor. 
+
+I have not found a solution yet.
+
+---
+
+# Changelog
+
+- 1.0 Working app and simulations for 1v1 and 2v2
